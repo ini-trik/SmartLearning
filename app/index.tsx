5c1c80c0306a1f5d983e3cc2e9297d37ea662d81
Fahmousss <09031382227177@student.unsri.ac.id>
2024-11-12 22:29:23 +0700

    Assign to @ini_trik

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
 import { Stack, Link } from 'expo-router';
-import { View } from 'react-native';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { Logo } from '~/components/Logo';
 import MessageCardList from '~/components/MessageCard/MessageCardList';
-import { ScreenContent } from '~/components/ScreenContent';
 import SearcInput from '~/components/SearcInput';
 
 export default function Home() {
---
+++ app/signIn.tsx
@@ -0,0 +1,8 @@
+/**
+ * Sign in page
+ * @author: @fahmousss
+ * @version: 1.0.0
+ * @since: 2024-11-12
+ * @assigned_to : @ini_trik
+ * @description : Sign in page
+ **/
