0747c0575481955273f46acf666aeb13ac9c43e9
Fahmousss <09031382227177@student.unsri.ac.id>
2024-11-13 16:15:25 +0700

    Connect User Avatar to Streamchat

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -3,8 +3,15 @@ import { View, Text } from 'react-native';
 import { Channel, ChannelList, MessageInput, MessageList } from 'stream-chat-expo';
 
 import { router } from 'expo-router';
+import { useAuth } from '~/providers/AuthProvider';
 
 export default function Main() {
-  return <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />;
+  const { user } = useAuth();
+  return (
+    <ChannelList
+      filters={{ members: { $in: user?.id ? [user.id] : [] } }}
+      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+    />
+  );
 }
 // Password chO4XOeEwziSCzix
--- providers/ChatProviders.tsx
+++ providers/ChatProviders.tsx
@@ -3,6 +3,7 @@ import { ActivityIndicator } from 'react-native';
 import { StreamChat } from 'stream-chat';
 import { OverlayProvider, Chat } from 'stream-chat-expo';
 import { useAuth } from './AuthProvider';
+import { supabase } from '~/lib/supabase';
 
 const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY!);
 export default function ChatProviders({ children }: PropsWithChildren) {
@@ -18,7 +19,7 @@ export default function ChatProviders({ children }: PropsWithChildren) {
         {
           id: profile.id,
           name: profile.full_name,
-          image: 'https://i.imgur.com/fR9Jz14.png',
+          image: supabase.storage.from('avatars').getPublicUrl(profile.avatar_url).data.publicUrl,
         },
         client.devToken(profile.id)
       );
