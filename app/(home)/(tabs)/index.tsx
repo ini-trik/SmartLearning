b82e4b8a88d6ac5e15131f476fb6afce92a1146e
Fahmousss <09031382227177@student.unsri.ac.id>
2024-11-14 17:37:45 +0700

    Add chat search and Update to SDK 52

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import { Channel, ChannelAvatar, ChannelList, MessageInput, MessageList } from 'stream-chat-expo';
 
 import { router } from 'expo-router';
 import { useAuth } from '~/providers/AuthProvider';
 import { supabase } from '~/lib/supabase';
+import SearcInput from '~/components/SearcInput';
 
 export default function Main() {
   const { user } = useAuth();
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState('');
+
   useEffect(() => {
     const fetchUsers = async () => {
       let { data: profiles } = await supabase.from('profiles').select('*').neq('id', user?.id); //Exclude current user
@@ -18,11 +21,33 @@ export default function Main() {
   }, [user?.id]);
 
   return (
-    <ChannelList
-      filters={{ members: { $in: user?.id ? [user.id] : [] } }}
-      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
-      PreviewAvatar={({ channel }) => <ChannelAvatar channel={channel} />}
-    />
+    <>
+      <TextInput
+        value={search}
+        onChangeText={(text) => {
+          setSearch(text);
+          console.log(text);
+        }}
+        autoCapitalize="none"
+      />
+      <ChannelList
+        filters={{
+          members: { $in: [user?.id!] },
+          ...(search
+            ? {
+                'member.user.name': { $autocomplete: search },
+              }
+            : {}),
+        }}
+        onSelect={async (channel) => {
+          //   router.push(`/channel/${channel.cid}`);
+          let sort = { user_id: 'asc' };
+          //   console.log(await channel.queryMembers({}, sort, {}));
+          console.log(channel.data?.displayName);
+        }}
+        PreviewAvatar={({ channel }) => <ChannelAvatar channel={channel} />}
+      />
+    </>
   );
 }
 // Password chO4XOeEwziSCzix
